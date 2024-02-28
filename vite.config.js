f8a29b142ac8b409a6ac852d4cd93adf022c2e67
Kaif-Shariff <kaif.shariff1234@gmail.com>
2024-02-28 22:22:51 +0530

    Solved Deployment Error

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      '/ex':{
-        target: 'http://localhost:3000/ex',
-        changeOrigin: true,
-        // rewrite: (path)=> path.replace
-      }
-    }
-  }
 })
